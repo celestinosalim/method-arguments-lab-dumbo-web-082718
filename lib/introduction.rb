09ddaa9358b33ecd65fac99f68c3b2f28433ad1a
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 00:00:10 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ def introduction name
 end
 
 def introduction_with_language name, language
-  puts "Hi my name is #{name} and i am learning to program in #{language}."
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
 
 end
